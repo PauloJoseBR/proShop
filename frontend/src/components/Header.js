41e21b39c1584162a191d67a0041b0bfe5e15690
paulotec96@gmail.com <Paulo>
2021-05-03 18:13:02 -0300

    Improving formatting

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,16 +5,23 @@ const Header = () => {
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-                <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+            <Container>
                 <Navbar.Brand href='/ProShop'>ProShop</Navbar.Brand>
                 <Navbar.Toggle aria-controls='basic-navbar-nav' />
                 <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className="mr-auto">
-                        <Nav.Link href='/cart'>Cart</Nav.Link>
-                        <Nav.Link href='/cart'>Sign In</Nav.Link>                        
+                    <Nav className="ml-auto">
+                        <Nav.Link href='/cart'>
+                            <i className="fas fa-shopping-cart"></i>
+                            Cart
+                        </Nav.Link>
+                        <Nav.Link href='/cart'>
+                            <i className="fas fa-shopping-cart"></i>
+                            Sign In
+                        </Nav.Link>                        
                     </Nav>
 
                 </Navbar.Collapse>
+             </Container>
             </Navbar>
         </header>
     )
