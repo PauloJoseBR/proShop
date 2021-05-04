655ab3b8c6d8bd9bb525b526392b62e9fa11edfd
paulo <paulotec96@gmail.com>
2021-05-04 17:11:04 -0300

    Adding new scripts plus refactoring

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -15,14 +15,14 @@ const HomeScreen = () => {
        }
 
        fetchProducts()
-    }, [])
+    })
 
     return (
         <>
             <h1>Latest Products</h1>
             <Row>
                 {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} lg={4} xl={3}>
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                         <Product product={product} />
                     </Col>
                 ))}
