bb4d0f4a865af836cfbddd001c54a96dd474dbfa
paulo <paulotec96@gmail.com>
2021-05-05 00:38:10 -0300

    Fetching from database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
 import connectDb from './config/db.js'
 import colors from 'colors'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -14,16 +15,8 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
+app.use('/api/products', productRoutes)
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
 
 const PORT = process.env.PORT || 5000
 
